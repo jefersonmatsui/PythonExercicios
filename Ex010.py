3fbcdce2ee3c7c645600c0bf94f8522b8c86d62e
Jeferson Matsui <50876944+jefersonmatsui@users.noreply.github.com>
2022-02-18 20:18:49 -0300

    Add files via upload

---
+++ Ex010.py
@@ -0,0 +1,11 @@
+# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar
+
+real = float(input('Quanto dinheiro você tem na carteira: R$'))
+dolar = real / 3.27
+print('Com R${:.2f}, você pode comprar U${:.2f}'.format(real, dolar))
+
+"""
+
+
+
+"""
