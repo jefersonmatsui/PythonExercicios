75e6c7d5c94aed072c46e486a401ff0c9df2882e
Jeferson Matsui <50876944+jefersonmatsui@users.noreply.github.com>
2022-02-25 20:08:24 -0300

    Add files via upload

---
+++ Aula010_Condições_Parte_1.py
@@ -0,0 +1,25 @@
+#vamos aprender como utilizar estruturas condicionais
+# simples e compostas nos seus programas em Python.
+
+'''
+Condição
+
+if carro.esquerda():
+    bloco True
+else:
+    bloco False
+
+tempo = int(input('Quantos anos tem seu carro? '))
+if tempo <= 3:
+    print('Carro novo')
+else:
+    print('Carro velho')
+print('--FIM--')
+
+#Condição simplificada
+tempo = int(input('Quantos anos tem seu carro? '))
+print('Carro novo' if tempo <= 3 else'Carro velho')
+print('--FIM--')
+'''
+
+
