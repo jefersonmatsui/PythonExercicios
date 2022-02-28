c536b21bc251be05951a7a8f1636c3f99df4b227
Jeferson Matsui <50876944+jefersonmatsui@users.noreply.github.com>
2022-02-28 20:15:54 -0300

    Add files via upload

--- Aula010_Condições_Parte_1.py
+++ Aula010_Condições_Parte_1.py
@@ -22,4 +22,20 @@ print('Carro novo' if tempo <= 3 else'Carro velho')
 print('--FIM--')
 '''
 
+'''
+nome = str(input('Qual é o seu nome? '))
+if nome == 'Gustavo':
+    print('Que nome lindo você tem!')
+else:
+    print('Seu nome é tão normal!')
+print('Bom dia, {}!'.format(nome))
+'''
 
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1 + n2) / 2
+print('A sua média foi {:.1f}'.format(m))
+if m >= 6.0:
+    print('Sua média foi boa! PARABÉNS!')
+else:
+    print('Sua média foi ruim!, ESTUDE MAIS!')
